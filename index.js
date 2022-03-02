cb8397f1f468a1272df4e1a7e6097a3b150cb61d
Honorine Ndom <honorinendzah@gmail.com>
2022-03-02 12:56:19 -0800

    modified index.js

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const { ApolloServer } = require('apollo-server');
 const gql =require ('graphql-tag');
 const mongoose = require('mongoose');   
 
+const Post = require('./models/Post');
 const { MONGODB } = require('./config.js')
 
+
 const typeDefs = gql`
+type Post{
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+}
    type Query{
-       sayHi: String!
+      getPosts: [Post] 
    }
-   `
+   `;
 
    const resolvers = {
        Query: {

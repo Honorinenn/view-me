95b72bf2af78a97834cc1316f0f712789b8d6f7b
Honorine Ndom <honorinendzah@gmail.com>
2022-03-10 05:14:03 -0800

    changes done on config.js and index.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     MONGODB:
-     'mongodb+srv://Merngis:SQ7yKfUqrgOidekR@cluster0.pandv.mongodb.net/Merngis?retryWrites=true&w=majority',
-    SECRET_KEY: 'some very secret key'
+     'mongodb+srv://Merngis:yRKia4kyGHV5C3Vv@cluster0.pandv.mongodb.net/Merngis?retryWrites=true&w=majority'
+            
 };
